009c1eb95afc271a9cad185636ce96266248fe9a
Andras <ruijiang5014@gmail.com>
2024-05-02 02:04:45 +0800

    error

---
+++ internal/errs/error.go
@@ -0,0 +1,17 @@
+package errs
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrPointerOnly = errors.New("orm: only support one pointer")
+)
+
+func NewErrUnsupportedExpr(expr any) error {
+	return fmt.Errorf("orm: unsupported expression: %s", expr)
+}
+func NewErrUnknownField(name string) error {
+	return fmt.Errorf("orm: unknown field: %s", name)
+}
