2797e89a6dc6f1859738899ce1f9fecf3f3a5f04
Andras <ruijiang5014@gmail.com>
2024-05-02 16:08:33 +0800

    model

--- model.go
+++ model.go
@@ -3,7 +3,13 @@ package go_orm
 import (
 	"github.com/Andras5014/go-orm/internal/errs"
 	"reflect"
+	"strings"
+	"sync"
 	"unicode"
+)
+
+const (
+	tagKeyColumn = "column"
 )
 
 type model struct {
@@ -15,8 +21,65 @@ type field struct {
 	colName string
 }
 
+//var defaultRegistry = &registry{
+//	models: make(map[reflect.Type]*model),
+//}
+
+// registry 元数据注册中心
+type registry struct {
+	models sync.Map
+	//lock   sync.RWMutex
+	//models map[reflect.Type]*model
+}
+
+func newRegistry() *registry {
+	return &registry{}
+}
+
+func (r *registry) get(entity any) (*model, error) {
+	typ := reflect.TypeOf(entity)
+
+	m, ok := r.models.Load(typ)
+
+	if ok {
+		return m.(*model), nil
+	}
+
+	var err error
+	m, err = r.parseModel(entity)
+	if err != nil {
+		return nil, err
+	}
+	r.models.Store(typ, m)
+	return m.(*model), nil
+}
+
+//func (r *registry) get1(entity any) (*model, error) {
+//	typ := reflect.TypeOf(entity)
+//	r.lock.RLock()
+//	m, ok := r.models[typ]
+//	r.lock.RUnlock()
+//	if ok {
+//		return m, nil
+//	}
+//	r.lock.Lock()
+//	defer r.lock.Unlock()
+//
+//	m, ok = r.models[typ]
+//	if ok {
+//		return m, nil
+//	}
+//	var err error
+//	m, err = r.parseModel(entity)
+//	if err != nil {
+//		return nil, err
+//	}
+//	r.models[typ] = m
+//	return m, nil
+//}
+
 // parseModel 解析实体类限制只能用一级指针
-func parseModel(entity any) (*model, error) {
+func (r *registry) parseModel(entity any) (*model, error) {
 	typ := reflect.TypeOf(entity)
 	if typ.Kind() != reflect.Ptr || typ.Elem().Kind() != reflect.Struct {
 		return nil, errs.ErrPointerOnly
@@ -26,14 +89,53 @@ func parseModel(entity any) (*model, error) {
 	fieldMap := make(map[string]*field, numField)
 	for i := 0; i < numField; i++ {
 		fd := typ.Field(i)
+		pairTag, err := r.parseTag(fd.Tag)
+		if err != nil {
+			return nil, err
+		}
+		colName := pairTag[tagKeyColumn]
+		if colName == "" {
+			// 如果没设置column
+			colName = underscoreName(fd.Name)
+		}
 		fieldMap[fd.Name] = &field{
-			colName: underscoreName(fd.Name),
+			colName: underscoreName(colName),
 		}
 	}
+	var tableName string
+	if tbname, ok := entity.(TableName); ok {
+		tableName = tbname.TableName()
+	}
+	if tableName == "" {
+		tableName = underscoreName(typ.Name())
+	}
 	return &model{
-		tableName: underscoreName(typ.Name()),
+		tableName: tableName,
 		fields:    fieldMap,
 	}, nil
+}
+
+type User struct {
+	ID uint64 `orm:"column"`
+}
+
+func (r *registry) parseTag(tag reflect.StructTag) (map[string]string, error) {
+	ormTag, ok := tag.Lookup("orm")
+	if !ok {
+		return map[string]string{}, nil
+	}
+	pairs := strings.Split(ormTag, ",")
+	res := make(map[string]string, len(pairs))
+	for _, pair := range pairs {
+		segs := strings.Split(pair, ":")
+		if len(segs) != 2 {
+			return nil, errs.NewErrInvalidTagContent(pair)
+		}
+		key := segs[0]
+		val := segs[1]
+		res[key] = val
+	}
+	return res, nil
 }
 
 // underscoreName 将驼峰命名转换为下划线命名
--- model_test.go
+++ model_test.go
@@ -3,6 +3,7 @@ package go_orm
 import (
 	"github.com/Andras5014/go-orm/internal/errs"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -47,14 +48,155 @@ func Test_parseModel(t *testing.T) {
 			wantErr: errs.ErrPointerOnly,
 		},
 	}
-
+	r := newRegistry()
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			gotModel, err := parseModel(tc.entity)
+			gotModel, err := r.parseModel(tc.entity)
 			if err != nil {
 				return
 			}
 			assert.Equal(t, tc.wantModel, gotModel)
 		})
 	}
+}
+
+func TestRegistry_get(t *testing.T) {
+	testCases := []struct {
+		name      string
+		entity    any
+		wantModel *model
+		wantErr   error
+	}{
+		{
+			name:   "pointer",
+			entity: &TestModel{},
+			wantModel: &model{
+				tableName: "test_model",
+				fields: map[string]*field{
+					"Id":        &field{colName: "id"},
+					"FirstName": &field{colName: "first_name"},
+					"LastName":  &field{colName: "last_name"},
+					"Age":       &field{colName: "age"},
+				},
+			},
+		},
+		{
+			name: "tag",
+			entity: func() any {
+				type TagTable struct {
+					FirstName string `orm:"column:first_name_t"`
+				}
+				return &TagTable{}
+			}(),
+			wantModel: &model{
+				tableName: "tag_table",
+				fields: map[string]*field{
+					"FirstName": {
+						colName: "first_name_t",
+					},
+				},
+			},
+		},
+		{
+			name: "empty column",
+			entity: func() any {
+				type TagTable struct {
+					FirstName string `orm:"column:"`
+				}
+				return &TagTable{}
+			}(),
+			wantModel: &model{
+				tableName: "tag_table",
+				fields: map[string]*field{
+					"FirstName": {
+						colName: "first_name",
+					},
+				},
+			},
+		},
+		{
+			name: "column only",
+			entity: func() any {
+				type TagTable struct {
+					FirstName string `orm:"column"`
+				}
+				return &TagTable{}
+			}(),
+			wantErr: errs.NewErrInvalidTagContent("column"),
+		},
+		{
+			name: "ignore tag",
+			entity: func() any {
+				type TagTable struct {
+					FirstName string `orm:"123:123"`
+				}
+				return &TagTable{}
+			}(),
+			wantModel: &model{
+				tableName: "tag_table",
+				fields: map[string]*field{
+					"FirstName": {
+						colName: "first_name",
+					},
+				},
+			},
+		},
+		{
+			name:   "table name",
+			entity: &CustomTableName{},
+			wantModel: &model{
+				tableName: "custom_table_name_t",
+				fields: map[string]*field{
+					"FirstName": {
+						colName: "first_name",
+					},
+				},
+			},
+		},
+		{
+			name:   "table name ptr",
+			entity: &CustomTableNamePtr{},
+			wantModel: &model{
+				tableName: "custom_table_name_ptr",
+				fields: map[string]*field{
+					"FirstName": {
+						colName: "first_name",
+					},
+				},
+			},
+		},
+	}
+	r := newRegistry()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			m, err := r.get(tc.entity)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantModel, m)
+			//assert.Equal(t, tc.cacheSize, r.models.Range)
+
+			typ := reflect.TypeOf(tc.entity)
+			model, ok := r.models.Load(typ)
+			assert.True(t, ok)
+			assert.Equal(t, tc.wantModel, model)
+		})
+	}
+}
+
+type CustomTableName struct {
+	FirstName string
+}
+
+func (c CustomTableName) TableName() string {
+	return "custom_table_name_t"
+}
+
+type CustomTableNamePtr struct {
+	FirstName string
+}
+
+func (c *CustomTableNamePtr) TableName() string {
+	return "custom_table_name_ptr"
 }
